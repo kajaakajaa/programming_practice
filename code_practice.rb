0ffdbd7983a6422b18457610ca42b744377bacef
kajaa16 <rwqxh8703@yahoo.co.jp>
2020-01-09 15:50:29 +0900

    first code_practice

---
+++ code_practice.rb
@@ -0,0 +1,112 @@
+# class Bank
+#   def initialize(password)
+#     @password = password
+#   end
+#   def correct_password?(entered_password)
+#     password == entered_password
+#   end
+
+#   private
+#   def password
+#     @password
+#   end
+# end
+
+# bank = Bank.new("保毛")
+# puts bank.correct_password?("保毛")
+
+# i = 2
+# while i < 300
+#   puts "i"
+#   i **=2
+# end
+
+# i = 0
+#   until i >= 10 # until =>ではない限りの意味
+#     puts i
+#     i += 1
+#   end
+
+# for i in 1..15 #1も15も含む
+#   puts i
+# end
+
+# 3. times do
+#   puts "hi"
+# end
+
+# 3. times do |i|
+#   puts "hi"
+# end
+
+# 1.upto (2) do
+#   puts "Hi there!"
+# end
+
+# 1.upto (10) do |i|
+#   puts i
+# end
+
+# 10.downto (7) do |i|
+#   puts i
+# end
+
+# 10.times do |i|
+#   if i == 5
+#     break
+#   else
+#     puts i
+#   end
+# end
+
+# 10.times do |i|
+#   if i == 5
+#     next
+#   else
+#     puts i
+#   end
+# end
+
+# PI = 3.14
+# puts PI.to_s + PI.to_s
+
+# total = 0
+# 10.times do |i|
+#   total += 1
+#   puts i.to_s + "回目、total =" + total.to_s
+#   if total == 5
+#     redo
+#   end
+# end
+
+# elems = ["ruby","python","perl"]
+# puts elems
+
+# elems = ["ruby","python","perl"]
+# puts elems[2]
+
+# foods = ["rice","bread","soup","milk"]
+# puts foods.length
+
+# foods = ["rice","bread","soup","milk"]
+# for value in foods
+#   puts value
+# end
+
+# food = {"name" => "チョコレート", "type" => "お菓子"}
+# puts food["name"]
+
+# food = {"name" => "チョコレート", "type" => "お菓子"}
+# food.each do |key,val|
+#   puts key + ": " + val
+# end
+
+# def hello
+#   puts "こんちわ"
+#   return "haisai"
+#   puts "さいなら"
+# end
+# # hello
+# kajaa = hello
+# puts kajaa
+
